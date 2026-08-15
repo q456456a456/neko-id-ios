c570dde1d3e6aed4b3987c47f218566f26e2cc9f
Amadeus <1210990655@qq.com>
2026-08-15 17:34:58 +0800

    Initial Commit

---
+++ neko-id/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  neko-id
+//
+//  Created by Amadeus on 2026/8/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ neko-id/neko_idApp.swift
@@ -0,0 +1,17 @@
+//
+//  neko_idApp.swift
+//  neko-id
+//
+//  Created by Amadeus on 2026/8/15.
+//
+
+import SwiftUI
+
+@main
+struct neko_idApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
